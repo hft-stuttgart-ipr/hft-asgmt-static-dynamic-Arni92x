2c2ac866715cbc366c6ffbbeb040fbeed3970354
Arni92x <62644542+Arni92x@users.noreply.github.com>
2021-04-25 13:38:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Do it!
+const express = require("express");
+const path = require("path");
+
+const port = process.env.Port || 3000 ;
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+     res.render("pages/index");
+});
+
+app.listen(port, ()=>{
+    console.log('Server running on port: ${port}');
+})
